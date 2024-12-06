44f721c5ba7332378082b3d6ad87c686aee1d2ac
Sahil Man Singh <sahil.singh@javra.com>
2024-12-06 17:54:55 +0545

    two sum added

---
+++ LeetCodeperday/LeetCodeperday/Easy/TwoSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeperday.Easy
+{
+    public class TwoSum
+    {
+        int[] nums = [3, 2, 4];
+        int target = 6;
+
+        public int[] TwoSumFunc()
+        {
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            int[] arrayreturn = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+
+                if (keyValuePairs.ContainsKey(diff)) {
+                    
+                    return new int[] { keyValuePairs[diff],i };
+                }
+
+                keyValuePairs.Add(nums[i], i);
+            }
+            return new int[] { 0,0};
+        }
+    }
+}
---
+++ LeetCodeperday/LeetCodeperday/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCodeperday.Easy;
+
+TwoSum sum = new TwoSum();
+var test = sum.TwoSumFunc();
+var sdf = "";
